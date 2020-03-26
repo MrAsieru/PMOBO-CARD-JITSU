fda21878d0ee4732d522f84a5d72f49663acbcec
MrAsieru <asiescp@gmail.com>
2020-03-26 12:41:23 +0100

    Karta guztiak garatu

---
+++ CardJitsu/src/cardJitsu/Karta.java
@@ -0,0 +1,5 @@
+package cardJitsu;
+
+public class Karta {
+
+}
---
+++ CardJitsu/src/cardJitsu/KartaGuztiak.java
@@ -0,0 +1,19 @@
+package cardJitsu;
+
+import java.util.ArrayList;
+
+public class KartaGuztiak {
+	private static ArrayList<Karta> lista;
+	
+	public KartaGuztiak() {
+	}
+	
+	public static void jsonetikKartetara() {
+		JSONParser jsonP = new JSONParser();
+		
+	}
+	
+	public static Karta getKarta(int pPos) {
+		
+	}
+}
