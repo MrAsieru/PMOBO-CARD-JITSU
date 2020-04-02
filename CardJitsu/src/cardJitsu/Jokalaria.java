38d4849f3ffc2105772eb38cc28e98f9afad1b34
miriber <miriambergaz@iesbotikazarbhi.net>
2020-04-02 19:25:46 +0200

    Jokalaria

--- CardJitsu/src/cardJitsu/Jokalaria.java
+++ CardJitsu/src/cardJitsu/Jokalaria.java
@@ -1,5 +1,22 @@
 package cardJitsu;
 
 public abstract class Jokalaria {
+		//atributuak
+		private String izena;
+		private ListaKartak gordetakoKartak;
+		private ListaKartak jolastekoKartak;
+
+		//metodo eraikitzailea
+		public Jokalaria (String pIzena) {
+			this.izena=pIzena;
+			this.gordetakoKartak= new ListaKartak();
+			this.jolastekoKartak= new ListaKartak();
+		}
+		
+		//gainerako metodoak
+		protected ListaKartak getgordetakoKartak()
+		  {
+			return this.gordetakoKartak;
+		  }
 
 }
