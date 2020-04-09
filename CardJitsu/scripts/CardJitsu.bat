fe1a2f87b652894b3bd3c716322159d4605bb30b
MrAsieru <asiescp@gmail.com>
2020-04-09 19:21:39 +0200

    Kontsolan nola ikusiko den aldatzea

--- CardJitsu/scripts/CardJitsu.bat
+++ CardJitsu/scripts/CardJitsu.bat
@@ -1,20 +1,36 @@
-@echo off
-echo [Jokalari1]:
-echo E: #### K: ####
-echo E: #### K: ####
-echo ----------------
-echo [Jokalari2]:
-echo E: #### K: ####
-echo E: #### K: ####
-echo ----------------
-echo Zure txanda [Jokalari2]:
-echo [1] E: #### B: ## K: ###### Ef: Beste jokalariari sua karta bat kendu
-echo [2] E: #### B: ## K: ###### Ef: Ezer
-echo [3] E: #### B: ## K: ###### Ef: Hurrengo txandan 2 gehitu
-echo [4] E: #### B: ## K: ###### Ef: Ezer
-echo [5] E: #### B: ## K: ###### Ef: Ezer
-echo --Karta aukeratzerakoan--
-echo ===========================================================================
-echo [Jokalari1]: E: #### B: ## K: ###### Ef: Beste jokalariari sua karta bat kendu
-echo [Jokalari2]: E: #### B: ## K: ###### Ef: Ezer
-echo [Jokalari#] irabazi du, --efektuaren mezua--
+Ongi etorri CardJitsu bideojokura
+  ____              _         _ _ _             
+ / ___|__ _ _ __ __| |       | (_) |_ ___ _   _ 
+| |   / _` | '__/ _` |    _  | | | __/ __| | | |
+| |__| (_| | | | (_| |   | |_| | | |_\__ \ |_| |
+ \____\__,_|_|  \__,_|    \___/|_|\__|___/\__,_|
+----------------
+Sartu zure izena:
+EHU
+----------------
+Aukeratu zailtasuna:
+[1] Erraza
+[2] Ertaina
+[3] Zaila
+----------------
+[Jokalari1]:
+E: #### K: ####
+E: #### K: ####
+----------------
+[Jokalari2]:
+E: #### K: ####
+E: #### K: ####
+----------------
+Zure txanda [Jokalari1]:
+[1] E: #### B: ## K: ###### Ef: <Efektuaren 1.deskripzioa>
+[2] E: #### B: ## K: ###### Ef: Ezer
+[3] E: #### B: ## K: ###### Ef: <Efektuaren 1.deskripzioa>
+[4] E: #### B: ## K: ###### Ef: Ezer
+[5] E: #### B: ## K: ###### Ef: Ezer
+----------------
+===========================================================================
+[Jokalari1]: E: #### B: ## K: ###### Ef: <Efektuaren 1.deskripzioa>
+[Jokalari2]: E: #### B: ## K: ###### Ef: Ezer
+[Jokalari#] irabazi du txanda, <Efektuaren 2.deskripzioa>
+----------------
+Zorionak! [Jokalari#] irabazi egin du partida!
