9f478e71668177daba6ac5549bc4bdb612f04321
miriber <miriambergaz@iesbotikazarbhi.net>
2020-05-04 13:32:09 +0200

    Test Kartak

---
+++ CardJitsu/Probak/cardjitsu/KartaBereziaTest.java
@@ -0,0 +1,41 @@
+package cardjitsu;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class KartaBereziaTest {
+	KartaBerezia k1;
+	@Before
+	public void setUp() throws Exception {
+		k1=new KartaBerezia (ElementuMota.URA,5,KoloreMota.HORIA,EfektuMota.KENDUGORRIBAT,"Deskribapena");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		k1=null;
+	}
+
+	@Test
+	public void testImprimatu() {
+		k1.imprimatu();
+	}
+
+	@Test
+	public void testKartaBerezia() {
+		assertNotNull(k1);
+	}
+
+	@Test
+	public void testGetEfektua() {
+		assertEquals(k1.getEfektua(),EfektuMota.KENDUGORRIBAT);
+	}
+
+	@Test
+	public void testGetDeskripzioa() {
+		assertEquals(k1.getDeskripzioa(),"Deskribapena");
+	}
+
+}
