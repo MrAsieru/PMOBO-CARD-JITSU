cf43cb478d7f6a17eb1e8e93709de433be4c1e1d
Unai Hurtado <unaihurtado@users.noreply.github.com>
2020-05-10 21:11:00 +0200

    JokalariBot hobetu

--- CardJitsu/src/cardjitsu/JokalariaBot.java
+++ CardJitsu/src/cardjitsu/JokalariaBot.java
@@ -34,16 +34,52 @@ public class JokalariaBot extends Jokalaria {
 	{
 		
 		boolean aukeratuta = false;
-		Karta karta = null;
+		Karta karta = (Karta) new KartaNormala(ElementuMota.ELURRA,-1,KoloreMota.BERDEA);
 		float randomness = new Random().nextFloat();
-		//Elementu berdina kolore desberdina irabazi sahiatu
-		if(randomness<=0.75 & (sortuMatrizeKartakElemDes().size()!=0 || sortuMatrizeKartakElemBer().size()!=0)) 
+		float randomness1 = new Random().nextFloat();
+		
+		//Elementu berdina edo derberdina eta kolore desberdineko bi karta izatean, beste karta batekin irabazi ahala badu, hori bota
+		if(randomness1<=0.75 & (sortuMatrizeKartakElemDes().size()!=0 || sortuMatrizeKartakElemBer().size()!=0)) 
 		{
 			kartaErabaki(sortuMatrizeKartakElemDes());
 			kartaErabaki(sortuMatrizeKartakElemBer());
 		}
+		//Basic counter
+		if(randomness<=0.5  & karta.getBalioa()==-1 & irabaziNahiDu().size()==1 || irabaziNahiDu().size()==2) 
+		{
+			if(irabaziNahiDu().contains(ElementuMota.SUA)) 
+			{
+				for(int i=0;i<5;i++) 
+				{
+					if(lortuJolastekoKartaPosz(i).getElementua()==ElementuMota.SUA & karta.getBalioa()<=lortuJolastekoKartaPosz(i).getBalioa()) 
+					{
+						karta=lortuJolastekoKartaPosz(i);
+					}
+				}
+			}
+			else if(irabaziNahiDu().contains(ElementuMota.URA)) 
+			{
+				for(int i=0;i<5;i++) 
+				{
+					if(lortuJolastekoKartaPosz(i).getElementua()==ElementuMota.URA & karta.getBalioa()<=lortuJolastekoKartaPosz(i).getBalioa()) 
+					{
+						karta=lortuJolastekoKartaPosz(i);
+					}
+				}
+			}
+			else if(irabaziNahiDu().contains(ElementuMota.ELURRA)) 
+			{
+				for(int i=0;i<5;i++) 
+				{
+					if(lortuJolastekoKartaPosz(i).getElementua()==ElementuMota.ELURRA & karta.getBalioa()<=lortuJolastekoKartaPosz(i).getBalioa()) 
+					{
+						karta=lortuJolastekoKartaPosz(i);
+					}
+				}
+			}
+		}
 		// Ausazko karta
-		if(karta==null)
+		if(karta.getBalioa()==-1)
 		{
 			while(!aukeratuta) {
 				karta = this.lortuJolastekoKartaPosz(new Random().nextInt(5));
@@ -128,5 +164,45 @@ public class JokalariaBot extends Jokalaria {
 			}
 		}
 		return LisKartak;
+	}
+	
+	private ArrayList<ElementuMota> irabaziNahiDu() 
+	{
+		Jokalaria j = getNireJokalaria(); 
+		ArrayList<ElementuMota> elem = new  ArrayList<ElementuMota>();
+		ArrayList<ArrayList<Karta>> LisKartak = new ArrayList<ArrayList<Karta>>();
+		for(int i=0;i<j.gordetakoKartenKantitatea();i++) 
+		{
+			for(int x=0;x<j.gordetakoKartenKantitatea();x++) 
+			{
+				if(j.lortuGordetakoKartaPosz(i).getElementua()!=j.lortuGordetakoKartaPosz(x).getElementua() & x!=i) 
+				{
+					ArrayList<Karta> kartak = new ArrayList<Karta>();
+					kartak.add(lortuGordetakoKartaPosz(i));
+					kartak.add(lortuGordetakoKartaPosz(x));
+					LisKartak.add(kartak);
+				}
+			}	
+		}
+		
+		for(int i=0;i<LisKartak.size();i++) 
+		{
+			if(LisKartak.get(i).get(0).getElementua()==ElementuMota.SUA & LisKartak.get(i).get(1).getElementua()==ElementuMota.URA 
+					|| LisKartak.get(i).get(0).getElementua()==ElementuMota.URA & LisKartak.get(i).get(1).getElementua()==ElementuMota.SUA) 
+			{
+				if(!elem.contains(ElementuMota.ELURRA)) {elem.add(ElementuMota.ELURRA);}
+			}
+			if(LisKartak.get(i).get(0).getElementua()==ElementuMota.SUA & LisKartak.get(i).get(1).getElementua()==ElementuMota.ELURRA 
+					|| LisKartak.get(i).get(0).getElementua()==ElementuMota.ELURRA & LisKartak.get(i).get(1).getElementua()==ElementuMota.SUA) 
+			{
+				if(!elem.contains(ElementuMota.URA)) {elem.add(ElementuMota.URA);}
+			}
+			if(LisKartak.get(i).get(0).getElementua()==ElementuMota.ELURRA & LisKartak.get(i).get(1).getElementua()==ElementuMota.URA 
+					|| LisKartak.get(i).get(0).getElementua()==ElementuMota.URA & LisKartak.get(i).get(1).getElementua()==ElementuMota.ELURRA) 
+			{
+				if(!elem.contains(ElementuMota.SUA)) {elem.add(ElementuMota.SUA);}
+			}
+		}
+		return elem;
 	}
 }
